53525654b85ebe9be15ffcc4894fdeccf85c380a
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-04-14 16:59:15 +0000

    new project

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+    protected $fillable = ['name','email', 'password'];
     // public function request(){
     //     return $this->hasMany('App\Models\User');
     // }
