d25ce34e169ed3a5aa77ff76abef1ae695a04039
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-05-20 09:54:18 +0100

    add ressourcw route for demandes operatins

--- pro/routes/web.php
+++ pro/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\OfferController;
+use App\Http\Controllers\DemandeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,16 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::post('/create-offer', [OfferController::class,'create'])->name('create-offer');
 
 
 Route::get('/',[OfferController::class,'home'])->name('home');
+Route::get('/search',[OfferController::class,'search'])->name('offers.search');
 Route::get('/dashboard',[OfferController::class,'index'])->middleware(['auth'])->name('dashboard');
 Route::resource('offers', OfferController::class);
+Route::resource('demandes', demandeController::class);
+
 
 
 
